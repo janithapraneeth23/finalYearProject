46c0a88bf6b1d67f9adb23a4a0d116377c312361
janitha <janithapraneeth23@gmail.com>
2022-08-06 22:23:10 +0530

    init

---
+++ src/test/java/com/janitha/videoenhancer/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janitha.videoenhancer.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
